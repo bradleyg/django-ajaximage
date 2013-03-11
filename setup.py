72a1954da11db3b997a55a52e0c2186885bf9176
bradley griffiths <bradley.griffiths@gmail.com>
2013-03-11 17:50:33 +0000

    pip package

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=False,
     classifiers=[
-        'Development Status :: 1 - Beta',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
