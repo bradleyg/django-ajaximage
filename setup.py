316b2593ec64d347c3a60604d130d5f36362d7c0
bradley griffiths <bradley.griffiths@gmail.com>
2013-05-27 17:23:27 +0100

    use local jquery

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='django-ajaximage',
-    version='0.1.13',
+    version='0.1.14',
     description='Add ajax image upload functionality with a progress bar to file input fields within Django admin. Images are optionally resized.',
     long_description=readme,
     author="Bradley Griffiths",
