dc5e87f4a7bb1399951423c3a4236c58ab723665
Rohan de Swardt <rohanza@gmail.com>
2014-02-20 00:19:25 +0200

    change AjaxImageField to behave like standard django FileField

--- ajaximage/fields.py
+++ ajaximage/fields.py
@@ -1,27 +1,45 @@
 #-*- coding: utf-8 -*-
+from django.core.files.storage import default_storage
+from django.db.models.fields.files import FileDescriptor, FieldFile
 from django.db.models import Field
-from django.forms import widgets
-from ajaximage.widgets import AjaxImageEditor
 from django.conf import settings
+from .widgets import AjaxImageEditor
 
 
 class AjaxImageField(Field):
+
+    storage = default_storage
+    attr_class = FieldFile
+    descriptor_class = FileDescriptor
+
     def __init__(self, *args, **kwargs):
         upload_to = kwargs.pop('upload_to', '')
         max_height = kwargs.pop('max_height', 0)
         max_width = kwargs.pop('max_width', 0)
         crop = kwargs.pop('crop', False)
         crop = 1 if crop is True else 0
-        
-        if(crop is 1 and (max_height is 0 or max_width is 0)):
+
+        if crop is 1 and (max_height is 0 or max_width is 0):
             raise Exception('Both max_width and max_height are needed if cropping')
 
-        self.widget = AjaxImageEditor(upload_to=upload_to,
-                                       max_width=max_width,
-                                       max_height=max_height,
-                                       crop=crop)
-                                       
+        self.widget = AjaxImageEditor(
+            upload_to=upload_to,
+            max_width=max_width,
+            max_height=max_height,
+            crop=crop
+        )
         super(AjaxImageField, self).__init__(*args, **kwargs)
+
+    def contribute_to_class(self, cls, name, virtual_only=False):
+        super(AjaxImageField, self).contribute_to_class(cls, name, virtual_only)
+        setattr(cls, self.name, self.descriptor_class(self))
+
+    def get_prep_value(self, value):
+        """Returns field's value prepared for saving into a database."""
+        # Need to convert File objects provided via a form to unicode for database insertion
+        if value is None:
+            return None
+        return str(value)
 
     def get_internal_type(self):
         return "TextField"
