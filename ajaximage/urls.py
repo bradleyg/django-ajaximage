be56b640784163f7702c495aa9ae5c1f6bea8d1f
Rohan de Swardt <rohanza@gmail.com>
2014-02-20 00:29:05 +0200

    urls cleanup - remove unused param

--- ajaximage/urls.py
+++ ajaximage/urls.py
@@ -1,15 +1,16 @@
-try:# pre 1.6
+try:  # pre 1.6
     from django.conf.urls.defaults import url, patterns
 except ImportError:
     from django.conf.urls import url, patterns
 
-from ajaximage.views import ajaximage
-from ajaximage.forms import FileForm
+from .forms import FileForm
 
-
-urlpatterns = patterns('',
-    url('^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)', ajaximage, {
-        'form_class': FileForm,
-        'response': lambda name, url: url,
-    }, name='ajaximage'),
+urlpatterns = patterns(
+    '',
+    url(
+        '^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)',
+        'ajaximage.views.ajaximage',
+        {'form_class': FileForm},
+        name='ajaximage'
+    ),
 )
