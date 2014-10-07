adb2a81d51cfcc19a301ba1712c3cc25cc699785
Bradley Griffiths <bradley.griffiths@gmail.com>
2014-10-07 15:41:26 +0100

    Remove form class.

--- ajaximage/urls.py
+++ ajaximage/urls.py
@@ -3,14 +3,11 @@ try:  # pre 1.6
 except ImportError:
     from django.conf.urls import url, patterns
 
-from .forms import FileForm
-
 urlpatterns = patterns(
     '',
     url(
         '^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)',
         'ajaximage.views.ajaximage',
-        {'form_class': FileForm},
         name='ajaximage'
     ),
 )
