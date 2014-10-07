ab592199d1c8961fff17937f33be79e49fb924ad
Bradley Griffiths <bradley.griffiths@gmail.com>
2014-10-07 15:32:13 +0100

    JS rewrite.

--- ajaximage/static/ajaximage/js/ajaximage.js
+++ ajaximage/static/ajaximage/js/ajaximage.js
@@ -1,73 +1,153 @@
-var $ajaxImage = jQuery.noConflict()
+(function(){
 
-$ajaxImage(function(){
+    "use strict"
 
-  var attach = function($fileInput, upload_url, el){
+    var getCookie = function(name) {
+        var value = '; ' + document.cookie,
+            parts = value.split('; ' + name + '=')
+        if (parts.length == 2) return parts.pop().split(';').shift()
+    }
 
-    $fileInput.fileupload({
-      url: upload_url,
-      formData: {},
-      dataType: 'json',
-      paramName: 'file',
+    var request = function(method, url, data, headers, el, showProgress, cb) {
+        var req = new XMLHttpRequest()
+        req.open(method, url, true)
 
-      add: function(e, data){
-        $ajaxImage(el).attr('class', 'ajaximage progress-active')
-        
-        var regex = /jpg|jpeg|png|gif/i
-        
-        if( ! regex.test(data.files[0].type)){
-          $ajaxImage(el).attr('class', 'ajaximage form-active')
-          return alert('Incorrect image format. Allowed (jpg, gif, png).')
+        Object.keys(headers).forEach(function(key){
+            req.setRequestHeader(key, headers[key])
+        })
+
+        req.onload = function() {
+            cb(req.status, req.responseText)
         }
 
-        data.submit()
-      },
+        req.onerror = req.onabort = function() {
+            disableSubmit(false)
+            error(el, 'Sorry, failed to upload image.')
+        }
 
-      progress: function(e, data){
-        var progress = parseInt(data.loaded / data.total * 100, 10)
-        $ajaxImage(el).find('.bar').css({width: progress + '%'})
-      },
+        req.upload.onprogress = function(data) {
+            progressBar(el, data, showProgress)
+        }
 
-      error: function(e, data){
-        alert('Oops, file upload failed, please try again')
-        $ajaxImage(el).attr('class', 'ajaximage form-active')
-      },
+        req.send(data)
+    }
 
-      done: function(e, data){
-        $ajaxImage(el).find('.link').attr('href', data.result.url)
-        $ajaxImage(el).find('img').attr('src', data.result.url)
-        $ajaxImage(el).attr('class', 'ajaximage img-active')
-        $ajaxImage(el).find('input[type=hidden]').val(data.result.filename)
-        $ajaxImage(el).find('.bar').css({width: '0%'})
-      }
+    var parseJson = function(json) {
+        var data
+        try {data = JSON.parse(json)}
+        catch(e){ data = null }
+        return data
+    }
+
+    var progressBar = function(el, data, showProgress) {
+        if(data.lengthComputable === false || showProgress === false) return
+
+        var pcnt = Math.round(data.loaded * 100 / data.total),
+            bar  = el.querySelector('.bar')
+
+        bar.style.width = pcnt + '%'
+    }
+
+    var error = function(el, msg) {
+        el.className = 'ajaximage form-active'
+        el.querySelector('.file-path').value = ''
+        el.querySelector('.file-input').value = ''
+        alert(msg)
+    }
+
+    var update = function(el, data) {
+        var link = el.querySelector('.file-link'),
+            path = el.querySelector('.file-path'),
+            img  = el.querySelector('.file-img')
+
+        link.setAttribute('href', data.url)
+        path.value = data.filename
+        img.src = data.url
+
+        el.className = 'ajaximage img-active'
+        el.querySelector('.bar').style.width = '0%'
+    }
+
+    var concurrentUploads = 0
+    var disableSubmit = function(status) {
+        var submitRow = document.querySelector('.submit-row')
+        if( ! submitRow) return
+
+        var buttons = submitRow.querySelectorAll('input[type=submit]')
+
+        if (status === true) concurrentUploads++
+        else concurrentUploads--
+
+        ;[].forEach.call(buttons, function(el){
+            el.disabled = (concurrentUploads !== 0)
+        })
+    }
+
+    var upload = function(e) {
+        var el      = e.target.parentElement,
+            file    = el.querySelector('.file-input').files[0],
+            dest    = el.querySelector('.file-dest').value,
+            form    = new FormData(),
+            headers = {'X-CSRFToken': getCookie('csrftoken')},
+            regex  = /jpg|jpeg|png|gif/i
+
+        if( ! regex.test(file.type)){
+            return alert('Incorrect image format. Allowed (jpg, gif, png).')
+        }
+
+        disableSubmit(true)
+
+        form.append('file', file)
+
+        request('POST', dest, form, headers, el, false, function(status, json){
+            disableSubmit(false)
+
+            var data = parseJson(json)
+
+            switch(status) {
+                case 200:
+                    update(el, data)
+                    break
+                case 400:
+                case 403:
+                    error(el, data.error)
+                    break;
+                default:
+                    error(el, 'Sorry, could not upload image.')
+            }
+        })
+    }
+
+    var removeUpload = function(e) {
+        e.preventDefault()
+
+        var el = e.target.parentElement
+        el.querySelector('.file-path').value = ''
+        el.querySelector('.file-input').value = ''
+        el.className = 'ajaximage form-active'
+    }
+
+    var addHandlers = function(el) {
+        var input  = el.querySelector('.file-input'),
+            remove = el.querySelector('.file-remove'),
+            path   = el.querySelector('.file-path'),
+            status = (path.value === '') ? 'form' : 'img'
+
+        el.className = 'ajaximage ' + status + '-active'
+
+        remove.addEventListener('click', removeUpload, false)
+        input.addEventListener('change', upload, false)
+    }
+
+    document.addEventListener('DOMContentLoaded', function(e) {
+        ;[].forEach.call(document.querySelectorAll('.ajaximage'), addHandlers)
     })
-  }
 
-  var setup = function(el){
-    var upload_url = $ajaxImage(el).data('url')
-    var img_url = $ajaxImage(el).find('input[type=hidden]').val()
-    var $fileInput = $ajaxImage(el).find('input[type=file]')
-    
-    var class_ = (img_url === '') ? 'form-active' : 'img-active'
-    $ajaxImage(el).attr('class', 'ajaximage ' + class_)
-    
-    $ajaxImage(el).find('.remove').click(function(e){
-      e.preventDefault()
-      $ajaxImage(el).find('input[type=hidden]').val('')
-      $ajaxImage(el).attr('class', 'ajaximage form-active')
+    document.addEventListener('DOMNodeInserted', function(e){
+        if(e.target.tagName) {
+            var el = e.target.querySelector('.ajaximage')
+            if(el) addHandlers(el)
+        }
     })
 
-    attach($fileInput, upload_url, el)
-  }
-
-  $ajaxImage('.ajaximage').each(function(i, el){
-    setup(el)
-  })
-
-  $ajaxImage(document).bind('DOMNodeInserted', function(e) {
-    var el = $ajaxImage(e.target).find('.ajaximage').get(0)
-    var yes = $ajaxImage(el).length !== 0
-    if(yes) setup(el)
-  })
-
-})
+})()
