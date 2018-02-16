4e27d638014976c6a1240db3262530946d113f6a
Xavier Bloemen <XavierBloemen@users.noreply.github.com>
2018-02-16 10:49:25 +0100

    fix for Django 2.0

--- ajaximage/widgets.py
+++ ajaximage/widgets.py
@@ -1,7 +1,7 @@
 import os
 from django.forms import widgets
 from django.utils.safestring import mark_safe
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.core.files.storage import default_storage
 
 
